89d069910b6a75aee061a14461dd069e57d06f68
Real ChaiR <realrealchair@gmail.com>
2022-06-01 22:52:55 -0400

    🧱: spaces -> tabs

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const authenticate = require('./model/authenticate');
 // redirect console output to log files
 const fs = require("fs")
 const path = require("path");
-let logConsoleStream = fs.createWriteStream('./logs/console.log', {flags: 'a'});
-let logErrorStream = fs.createWriteStream('./logs/error.log', {flags: 'a'});
+let logConsoleStream = fs.createWriteStream('./logs/console.log', { flags: 'a' });
+let logErrorStream = fs.createWriteStream('./logs/error.log', { flags: 'a' });
 process.stdout.write = logConsoleStream.write.bind(logConsoleStream)
 process.stderr.write = logErrorStream.write.bind(logErrorStream)
 process.on('uncaughtException', function (ex) {
-    console.error(ex)
+	console.error(ex)
 });
 
 // server listener
@@ -25,224 +25,224 @@ var mongoHandler = require('./helpers/mongoHandler')
 
 // initial db connection
 mongoHandler.connectToServer((err, client) => {
-    if (err) console.log(err)
+	if (err) console.log(err)
 
-    // express setup
-    const app = express();
-    var http = require('http').createServer(app);
+	// express setup
+	const app = express();
+	var http = require('http').createServer(app);
 
-    // variables needed for twitch calls
-    const callbackUrl = process.env.CALLBACK_URL
-    const port = process.env.PORT || 6336
-    const twitchSigningSecret = process.env.TWITCH_SIGNING_SECRET
+	// variables needed for twitch calls
+	const callbackUrl = process.env.CALLBACK_URL
+	const port = process.env.PORT || 6336
+	const twitchSigningSecret = process.env.TWITCH_SIGNING_SECRET
 
-    // start listening for gets/posts
-    const listener = app.listen(port, () => {
-        console.log("Your app is listening on port " + listener.address().port);
-    })
+	// start listening for gets/posts
+	const listener = app.listen(port, () => {
+		console.log("Your app is listening on port " + listener.address().port);
+	})
 
-    // import more helper classes
-    var EventSub = require('./helpers/eventSub')
-    var FactionPoints = require('./helpers/factionPoints')
-    var AhogeManager = require('./helpers/ahoge')
+	// import more helper classes
+	var EventSub = require('./helpers/eventSub')
+	var FactionPoints = require('./helpers/factionPoints')
+	var AhogeManager = require('./helpers/ahoge')
 
-    // initialize faction overview socket
-    const io = require('socket.io')(listener, {
-        path: '/socket',
-        rejectUnauthorized: 'false'
-    })
+	// initialize faction overview socket
+	const io = require('socket.io')(listener, {
+		path: '/socket',
+		rejectUnauthorized: 'false'
+	})
 
-    // holds recent EventSub event IDs to prevent consuming duplicates
-    let recentNotifIds = new Set();
+	// holds recent EventSub event IDs to prevent consuming duplicates
+	let recentNotifIds = new Set();
 
-    // Welcome Screen
-    app.get("/", (req, res) => {
-        res.send("perish.");
-    });
+	// Welcome Screen
+	app.get("/", (req, res) => {
+		res.send("perish.");
+	});
 
-    // easter egg :3
-    app.get("/snocket", (req, res) => {
-        res.send("This is where Nexilitus goes genera421Oops")
-    })
+	// easter egg :3
+	app.get("/snocket", (req, res) => {
+		res.send("This is where Nexilitus goes genera421Oops")
+	})
 
-		app.get('/auth', async (req, res) => {
-			const url = new URL(req.query.clientUrl);
-			const authData = await authenticate(req.query);
+	app.get('/auth', async (req, res) => {
+		const url = new URL(req.query.clientUrl);
+		const authData = await authenticate(req.query);
 
-			res.cookie(`${authData.type}-token`, authData.access_token, {
-				httpOnly: true,
-				maxAge: authData.expires_in,
-				sameSite: 'Strict',
-			});
-
-			res.redirect(url);
+		res.cookie(`${authData.type}-token`, authData.access_token, {
+			httpOnly: true,
+			maxAge: authData.expires_in,
+			sameSite: 'Strict',
 		});
 
-    // Display active EventSub subscriptions (only in test mode)
-    if (process.env.TEST_ENV == "true") {
-    app.get('/listWebhooks', (req, res) => {
-        var createWebHookParams = {
-            host: "api.twitch.tv",
-            path: "helix/eventsub/subscriptions",
-            method: 'GET',
-            headers: {
-                "Client-ID": process.env.TWITCH_CLIENT_ID,
-                "Authorization": "Bearer " + process.env.TWITCH_APP_BEARER
-            }
-        };
-        var responseData = "";
-        var listRequest = https.request(createWebHookParams, (result) => {
-            result.setEncoding('utf8');
-            result.on('data', function (d) {
-                responseData = responseData + d;
-            })
-            .on('end', function (result) {
-                var responseBody = JSON.parse(responseData);
-                res.send(responseBody);
-            });
-        });
-        listRequest.on('error', (e) => { console.log("Error") });
-        listRequest.end()
-    });
-    }
+		res.redirect(url);
+	});
 
-    // verify signature of twitch even, check the "Verifying the event message" section of https://dev.twitch.tv/docs/eventsub/handling-webhook-events
-    const verifyTwitchSignature = (req, res, buf, encoding) => {
-        const messageId = req.header("Twitch-Eventsub-Message-Id");
-        const timestamp = req.header("Twitch-Eventsub-Message-Timestamp");
-        const messageSignature = req.header("Twitch-Eventsub-Message-Signature");
+	// Display active EventSub subscriptions (only in test mode)
+	if (process.env.TEST_ENV == "true") {
+		app.get('/listWebhooks', (req, res) => {
+			var createWebHookParams = {
+				host: "api.twitch.tv",
+				path: "helix/eventsub/subscriptions",
+				method: 'GET',
+				headers: {
+					"Client-ID": process.env.TWITCH_CLIENT_ID,
+					"Authorization": "Bearer " + process.env.TWITCH_APP_BEARER
+				}
+			};
+			var responseData = "";
+			var listRequest = https.request(createWebHookParams, (result) => {
+				result.setEncoding('utf8');
+				result.on('data', function (d) {
+					responseData = responseData + d;
+				})
+					.on('end', function (result) {
+						var responseBody = JSON.parse(responseData);
+						res.send(responseBody);
+					});
+			});
+			listRequest.on('error', (e) => { console.log("Error") });
+			listRequest.end()
+		});
+	}
 
-        const time = Math.floor(new Date().getTime() / 1000);
+	// verify signature of twitch even, check the "Verifying the event message" section of https://dev.twitch.tv/docs/eventsub/handling-webhook-events
+	const verifyTwitchSignature = (req, res, buf, encoding) => {
+		const messageId = req.header("Twitch-Eventsub-Message-Id");
+		const timestamp = req.header("Twitch-Eventsub-Message-Timestamp");
+		const messageSignature = req.header("Twitch-Eventsub-Message-Signature");
 
-        if (Math.abs(time - timestamp) > 600) {
-            // must be < 10 minutes
-            console.log(`Verification Failed: timestamp > 10 minutes. Message Id: ${messageId}.`);
-            throw new Error("Ignore this request.");
-        }
+		const time = Math.floor(new Date().getTime() / 1000);
 
-        if (!twitchSigningSecret) {
-            console.log(`Twitch signing secret is empty`);
-            throw new Error("Twitch signing secret is empty");
-        }
+		if (Math.abs(time - timestamp) > 600) {
+			// must be < 10 minutes
+			console.log(`Verification Failed: timestamp > 10 minutes. Message Id: ${messageId}.`);
+			throw new Error("Ignore this request.");
+		}
 
-        const computedSignature =
-            "sha256=" +
-            crypto
-                .createHmac("sha256", twitchSigningSecret)
-                .update(messageId + timestamp + buf)
-                .digest("hex");
+		if (!twitchSigningSecret) {
+			console.log(`Twitch signing secret is empty`);
+			throw new Error("Twitch signing secret is empty");
+		}
 
-        if (messageSignature !== computedSignature) {
-            //throw new Error("Invalid Signature.");
-            console.log("Verification failed");
-            res.status(403).send("Forbidden");
-        }
-    };
+		const computedSignature =
+			"sha256=" +
+			crypto
+				.createHmac("sha256", twitchSigningSecret)
+				.update(messageId + timestamp + buf)
+				.digest("hex");
 
-    // use verification function as express middleware
-    app.use(express.json({ verify: verifyTwitchSignature }));
+		if (messageSignature !== computedSignature) {
+			//throw new Error("Invalid Signature.");
+			console.log("Verification failed");
+			res.status(403).send("Forbidden");
+		}
+	};
 
-    // endpoint for handling EventSub events
-    app.post("/notification", async (req, res) => {
-        const messageType = req.header("Twitch-Eventsub-Message-Type");
-        // must reply with 200 status/verify before any processing
-        if (messageType === "webhook_callback_verification") {
-            console.log("Verifying Webhook");
-            return res.status(200).send(req.body.challenge);
-        } else {
-            res.status(200).end();
-        }
+	// use verification function as express middleware
+	app.use(express.json({ verify: verifyTwitchSignature }));
 
-        const { type } = req.body.subscription;
-        const { event } = req.body;
+	// endpoint for handling EventSub events
+	app.post("/notification", async (req, res) => {
+		const messageType = req.header("Twitch-Eventsub-Message-Type");
+		// must reply with 200 status/verify before any processing
+		if (messageType === "webhook_callback_verification") {
+			console.log("Verifying Webhook");
+			return res.status(200).send(req.body.challenge);
+		} else {
+			res.status(200).end();
+		}
 
-        if (req.header("Twitch-Eventsub-Message-Type") === "notification") {
-            // log event if in debug mode
-            if (process.env.DEBUG == "true") { console.log(type);console.log(event); }
+		const { type } = req.body.subscription;
+		const { event } = req.body;
 
-            // make sure this event has not been received already
-            if (!recentNotifIds.has(event.id)) {
-                // add event id to recents notif list
-                recentNotifIds.add(event.id);
+		if (req.header("Twitch-Eventsub-Message-Type") === "notification") {
+			// log event if in debug mode
+			if (process.env.DEBUG == "true") { console.log(type); console.log(event); }
 
-                // there's some kinda iffy race condition fucky wucky goin on in here
-                if (type != "stream.online" && type != "stream.offline" && type != "channel.channel_points_custom_reward_redemption.add") {
-                    AhogeManager.addXp(type,event)
-                }
-                // parse out the title of the redeem
-                else if (type === "channel.channel_points_custom_reward_redemption.add") {
-                    FactionPoints.parseFactionPoints(event);
-                }
-                else if(type == "stream.online"){
-                    AhogeManager.streamOnline()
-                }
-                else if(type == "stream.offline"){
-                    AhogeManager.streamOffline()
-                }
-            }
-        }
-        else if ( req.header("Twitch-Eventsub-Message-Type") === "revocation" ) {
-            console.log("EventSub Subscription Revoked")
-            recentNotifIds.clear();
-            EventSub.initializeRedeemSubscription(req.body.type);
-        }
-        else {
-            console.log(`Message type ${req.header("Twitch-Eventsub-Message-Type")} not recognized as command.`)
-            console.log(req)
-        }
-    });
+			// make sure this event has not been received already
+			if (!recentNotifIds.has(event.id)) {
+				// add event id to recents notif list
+				recentNotifIds.add(event.id);
 
-    // START EVENTSUB SUBSCRIPTION FOR CHANNEL REDEEMS
-    if (process.env.LISTEN_EVENTS == "true") {
-        EventSub.getEventSubSubscriptions();
-    }
+				// there's some kinda iffy race condition fucky wucky goin on in here
+				if (type != "stream.online" && type != "stream.offline" && type != "channel.channel_points_custom_reward_redemption.add") {
+					AhogeManager.addXp(type, event)
+				}
+				// parse out the title of the redeem
+				else if (type === "channel.channel_points_custom_reward_redemption.add") {
+					FactionPoints.parseFactionPoints(event);
+				}
+				else if (type == "stream.online") {
+					AhogeManager.streamOnline()
+				}
+				else if (type == "stream.offline") {
+					AhogeManager.streamOffline()
+				}
+			}
+		}
+		else if (req.header("Twitch-Eventsub-Message-Type") === "revocation") {
+			console.log("EventSub Subscription Revoked")
+			recentNotifIds.clear();
+			EventSub.initializeRedeemSubscription(req.body.type);
+		}
+		else {
+			console.log(`Message type ${req.header("Twitch-Eventsub-Message-Type")} not recognized as command.`)
+			console.log(req)
+		}
+	});
 
-    // ---------------------------- LIVE FACTION VIEW CODE ----------------------------
+	// START EVENTSUB SUBSCRIPTION FOR CHANNEL REDEEMS
+	if (process.env.LISTEN_EVENTS == "true") {
+		EventSub.getEventSubSubscriptions();
+	}
 
-    // client list to maintain
-    var clients = {};
+	// ---------------------------- LIVE FACTION VIEW CODE ----------------------------
 
-    // load the table that holds points
-    var db = mongoHandler.getDb()
-    const collection = db.collection('sprints')
+	// client list to maintain
+	var clients = {};
 
-    // handle new socket connections
-    io.on('connection', (socket) => {
-        var id = socket.id;
-        var ip = socket.request.connection.remoteAddress;
-        clients[id] = {socket:socket, ip:ip}
+	// load the table that holds points
+	var db = mongoHandler.getDb()
+	const collection = db.collection('sprints')
 
-        clients[id].socket.emit('connection', null);
+	// handle new socket connections
+	io.on('connection', (socket) => {
+		var id = socket.id;
+		var ip = socket.request.connection.remoteAddress;
+		clients[id] = { socket: socket, ip: ip }
 
-        clients[id].socket.on('disconnect', () => {
-            delete clients[id];
-        });
+		clients[id].socket.emit('connection', null);
 
-        clients[id].socket.on('connect_error', () => {
-            console.log(`Connection error detected from socket ${id} at IP: ${ip}`)
-        });
+		clients[id].socket.on('disconnect', () => {
+			delete clients[id];
+		});
 
-        // initialize faction values for new sockets
-        query = {"discordServer": process.env.DISCORD_SERVER_ID, "sprintEnd":{"$exists":false}}
-        collection.find(query).toArray(function(err,result){
-            if (err)
-                throw err;
+		clients[id].socket.on('connect_error', () => {
+			console.log(`Connection error detected from socket ${id} at IP: ${ip}`)
+		});
 
-            for (const faction of result) {
-                clients[id].socket.emit('update',{faction: faction["faction"], total: faction["total"]})
-            }
-        });
-    });
+		// initialize faction values for new sockets
+		query = { "discordServer": process.env.DISCORD_SERVER_ID, "sprintEnd": { "$exists": false } }
+		collection.find(query).toArray(function (err, result) {
+			if (err)
+				throw err;
 
-    // create change stream to emit point updates to all sockets
-    const pipeLine = [{$match:{'fullDocument.discordServer': process.env.DISCORD_SERVER_ID}}]
-    const changeStream = collection.watch(pipeLine,{fullDocument: 'updateLookup'});
-    changeStream.on('change', next => {
-        io.emit('update',{faction: next.fullDocument.faction, total: next.fullDocument.total})
-    })
+			for (const faction of result) {
+				clients[id].socket.emit('update', { faction: faction["faction"], total: faction["total"] })
+			}
+		});
+	});
 
-    app.use(express.static(path.join(__dirname, 'factionLiveView/build')))
-    app.get('/factionOverview', function (req,res) {
-        res.sendFile(path.join(__dirname,'factionLiveView/build','index.html'))
-    })
+	// create change stream to emit point updates to all sockets
+	const pipeLine = [{ $match: { 'fullDocument.discordServer': process.env.DISCORD_SERVER_ID } }]
+	const changeStream = collection.watch(pipeLine, { fullDocument: 'updateLookup' });
+	changeStream.on('change', next => {
+		io.emit('update', { faction: next.fullDocument.faction, total: next.fullDocument.total })
+	})
+
+	app.use(express.static(path.join(__dirname, 'factionLiveView/build')))
+	app.get('/factionOverview', function (req, res) {
+		res.sendFile(path.join(__dirname, 'factionLiveView/build', 'index.html'))
+	})
 })
