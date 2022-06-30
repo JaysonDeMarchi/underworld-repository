9bcb27d8ae3773d3904fb144996040732bdfa0fa
Real ChaiR <realrealchair@gmail.com>
2022-06-30 00:46:53 -0400

    🧹: Set export to a variable before exporting

--- packages/core-server/src/factionLiveView/src/leaderboard/factionList.js
+++ packages/core-server/src/factionLiveView/src/leaderboard/factionList.js
@@ -1,5 +1,7 @@
-export default [
+const factionList = [
     {factionName: 'creatures', total: 0},
     {factionName: 'undead', total: 0},
     {factionName: 'monsters', total: 0}
 ];
+
+export default factionList;
