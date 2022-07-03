94e5509857a51eed35718718c93040d68cbf132d
Real ChaiR <realrealchair@gmail.com>
2022-07-02 23:07:15 -0400

    🧹: Move websockets to new architecture

--- packages/core-server/src/index.js
+++ packages/core-server/src/index.js
@@ -15,16 +15,16 @@ process.on('uncaughtException', function (ex) {
 	console.error(ex);
 });
 
+// variables needed for twitch calls
+const port = process.env.PORT || 6336;
+
 // initial db connection
 mongoHandler.connectToServer((err) => {
 	if (err) console.log(err);
 
 	// express setup
 	const app = express();
-	require('http').createServer(app);
-
-	// variables needed for twitch calls
-	const port = process.env.PORT || 6336;
+	const server = require('http').createServer(app);
 
 	// Welcome Screen
 	app.get("/", (req, res) => {
@@ -51,15 +51,11 @@ mongoHandler.connectToServer((err) => {
 		});
 	});
 
-	// start listening for gets/posts
-	const listener = app.listen(port, () => {
-		console.log("Your app is listening on port " + listener.address().port);
-	});
-
-	// initialize faction overview socket
-	const io = require('socket.io')(listener, {
-		path: '/socket',
-		rejectUnauthorized: 'false'
+	fs.readdir(`${__dirname}/websockets/`, (_, websockets) => {
+		websockets.forEach((websocketName) => {
+			const websocket = require(`${__dirname}/websockets/${websocketName}`);
+			websocket.configure(server);
+		});
 	});
 
 	// START EVENTSUB SUBSCRIPTION FOR CHANNEL REDEEMS
@@ -67,47 +63,7 @@ mongoHandler.connectToServer((err) => {
 		EventSub.getEventSubSubscriptions();
 	}
 
-	// ---------------------------- LIVE FACTION VIEW CODE ----------------------------
-
-	// client list to maintain
-	var clients = {};
-
-	// load the table that holds points
-	var db = mongoHandler.getDb();
-	const collection = db.collection('sprints');
-
-	// handle new socket connections
-	io.on('connection', (socket) => {
-		var id = socket.id;
-		var ip = socket.request.connection.remoteAddress;
-		clients[id] = { socket: socket, ip: ip };
-
-		clients[id].socket.emit('connection', null);
-
-		clients[id].socket.on('disconnect', () => {
-			delete clients[id];
-		});
-
-		clients[id].socket.on('connect_error', () => {
-			console.log(`Connection error detected from socket ${id} at IP: ${ip}`);
-		});
-
-		// initialize faction values for new sockets
-		const query = { "discordServer": process.env.DISCORD_SERVER_ID, "sprintEnd": { "$exists": false } };
-		collection.find(query).toArray(function (err, result) {
-			if (err)
-				throw err;
-
-			for (const faction of result) {
-				clients[id].socket.emit('update', { faction: faction["faction"], total: faction["total"] });
-			}
-		});
-	});
-
-	// create change stream to emit point updates to all sockets
-	const pipeLine = [{ $match: { 'fullDocument.discordServer': process.env.DISCORD_SERVER_ID } }];
-	const changeStream = collection.watch(pipeLine, { fullDocument: 'updateLookup' });
-	changeStream.on('change', next => {
-		io.emit('update', { faction: next.fullDocument.faction, total: next.fullDocument.total });
+	server.listen(port, () => {
+		console.log(`Your app is listening on port ${port}`);
 	});
 });
