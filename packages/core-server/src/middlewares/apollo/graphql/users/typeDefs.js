8bb380b500fb14e8f0ecc0331e16ed43e9fab291
Real ChaiR <realrealchair@gmail.com>
2022-07-03 20:21:47 -0400

    🗻: Configure the Apollo Server Middleware
    Also configures the user typeDef since we needed some typeDef declared
    to setup the middleware.

---
+++ packages/core-server/src/middlewares/apollo/graphql/users/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+input UserInput {
+	discordId: String
+	discordNickname: String
+	faction: String
+	negative: Int
+	positive: Int
+	total: Int
+	twitchId: String
+	twitchUsername: String
+}
+
+type User {
+	discordServer: String
+	discordId: String
+	nickname: String
+	faction: String
+	negative: Int
+	positive: Int
+	total: Int
+	twitchId: String
+	twitchUsername: String
+}
+
+type Query {
+	getUser: User
+}
+
+type Mutation {
+	createUser(
+		user: UserInput
+	): User
+
+	deleteUser(
+		discordId: String
+		twitchId: String
+	): Boolean
+
+	updateUser(
+		user: UserInput
+	): User
+}
+`;
+
+module.exports = typeDefs;
